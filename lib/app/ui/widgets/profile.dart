759dd66be9f7225d63c1d48b204525d95e90318d
MinkseokSung <sms951591@gmail.com>
2023-06-20 21:10:49 +0900

    h
    h

--- lib/app/ui/widgets/profile.dart
+++ lib/app/ui/widgets/profile.dart
@@ -22,7 +22,7 @@ class Profile extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if(isMe)//g
+          if(isMe)//h
             ...[
               Container(
                 width: 50,
