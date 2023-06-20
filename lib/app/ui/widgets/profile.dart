e9eb9ec41792209c6cb04da8db5c9f02af4378df
MinkseokSung <sms951591@gmail.com>
2023-06-20 18:57:57 +0900

    test
    test

--- lib/app/ui/widgets/profile.dart
+++ lib/app/ui/widgets/profile.dart
@@ -22,7 +22,7 @@ class Profile extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.end,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          if(isMe)//
+          if(isMe)
             ...[
               Container(
                 width: 50,
